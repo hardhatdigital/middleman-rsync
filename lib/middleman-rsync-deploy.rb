fb88eafe3f34cd1b7d90f38e09e3fd566cc9d525
Bunts Thy Unholy <bunts@hhd.com.au>
2016-12-15 16:11:01 +1100

    Setting up sample CLI command

--- lib/middleman-rsync-deploy.rb
+++ lib/middleman-rsync-deploy.rb
@@ -2,6 +2,7 @@ require "middleman-core"
 require "middleman-rsync-deploy/commands"
 
 Middleman::Extensions.register :deploy do
-  require "rsync-deploy/extension"
+  require "middleman-rsync-deploy/extension"
+
   RsyncDeploy
 end
--- lib/middleman-rsync-deploy/extension.rb
+++ lib/middleman-rsync-deploy/extension.rb
@@ -1,7 +1,5 @@
-# Require core library
-require 'middleman-core'
+require "middleman-core"
 
-# Extension namespace
 class RsyncDeploy < ::Middleman::Extension
   option :server, nil, "Name of deployment server"
   option :path, nil, "Path on server to sync files"
@@ -9,20 +7,5 @@ class RsyncDeploy < ::Middleman::Extension
 
   def initialize(app, options_hash={}, &block)
     super
-
-    # Require libraries only when activated
-    # require 'necessary/library'
-
-    # set up your extension
-    # puts options.my_option
   end
-
-  def after_configuration
-    # Do something
-  end
-
-  # helpers do
-  #   def a_helper
-  #   end
-  # end
 end
