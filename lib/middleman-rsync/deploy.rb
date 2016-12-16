ad96b9619efcb34d6f9b48e607c0aa283ad9d89d
Bunts Thy Unholy <bunts@hhd.com.au>
2016-12-16 11:45:30 +1100

    Building out rsync process

--- lib/middleman-rsync/deploy.rb
+++ lib/middleman-rsync/deploy.rb
@@ -2,13 +2,29 @@ module Middleman
   module Rsync
     module_function
 
+    def ask(*args)
+      print(*args)
+      STDIN.gets.strip
+    end
+
     def deploy(app, environment)
       config = app.extensions[:deploy].options
       server = config["#{environment}_server".to_sym]
 
-      # Build
+      puts "Deploying to #{environment}: #{server}\n"
+
+      if not ["yes", "y"].include?(ask("OK? [Yes|y|Y|No|n|N] > ").downcase)
+        puts "Exiting."
+        exit(1)
+      end
+
+      puts "Building locally..."
+      run("middleman build") || exit(1)
+
+      puts "Running rsync..."
       # Rsync
-      # Report
+      
+      puts "Complete."
     end
   end
 end
